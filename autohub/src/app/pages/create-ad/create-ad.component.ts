b4d001eb25af286ab9fa238e294f58d414895352
gabrielkanelov` <gabrielkanelov2003@gmail.com`>
2025-08-18 17:20:11 +0300

    feat(autohub): add main pages, guards, services and pipes
    - Implemented core pages: home, catalog, details, login, register, profile, create-ad, edit-ad
    - Added auth and guest guards
    - Created ads and auth services
    - Added truncate pipe
    - Updated app config, routes and base components
    - Initial setup for core module

---
+++ autohub/src/app/pages/create-ad/create-ad.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-ad',
+  imports: [],
+  templateUrl: './create-ad.component.html',
+  styleUrl: './create-ad.component.scss'
+})
+export class CreateAdComponent {
+
+}
---
+++ autohub/src/app/pages/edit-ad/edit-ad.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-ad',
+  imports: [],
+  templateUrl: './edit-ad.component.html',
+  styleUrl: './edit-ad.component.scss'
+})
+export class EditAdComponent {
+
+}
